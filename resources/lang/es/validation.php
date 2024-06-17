b845e688719ff1e1f0d3d9cfd0cae0a070c64879
pv17028 <pv17028@ues.edu.sv>
2024-06-17 14:10:48 -0600

    Mensajes de validaciones

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -6,11 +6,19 @@ return [
     'image' => 'El campo :attribute debe ser una imagen.',
     'mimes' => 'El campo :attribute debe ser un archivo de tipo: :values.',
     'confirmed' => 'El campo :attribute no coincide con la confirmación.',
+    'email' => 'El campo :attribute debe ser una dirección de correo electrónico válida.',
     'attributes' => [
         'user_id' => 'usuario',
         'reason' => 'motivo',
         'password' => 'contraseña',
+        'email' => 'correoElectronico',
+        'username' => 'usuario',
         // ...
+    ],
+    'custom' => [
+        'email' => [
+            'required' => 'Necesitamos conocer tu dirección de correo electrónico.',
+        ],
     ],
     // otros mensajes de validación...
 ];
