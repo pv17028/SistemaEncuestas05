95bc650b0cbd9ea4871116aa3e2bb7d18e59f208
pv17028 <pv17028@ues.edu.sv>
2024-06-19 14:10:16 -0600

    Graficos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
